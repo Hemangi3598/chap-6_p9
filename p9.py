2c27d811400ee3cd5ab2e44b50393ef4e9057d92
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-18 17:27:58 +0530

    Add files via upload

---
+++ p9.py
@@ -0,0 +1,20 @@
+# pp and kp
+
+def show(a, b, c, d):
+	print(a, b, c, d)
+
+# positional par --> PP
+show(10, 20, 30, 40)
+show(20, 30, 10, 50)
+show(3, 5, 7, 8)
+
+# keyword par ---> KP
+show(a=21, b=89, c=9, d=99)
+show(b=20, d=87,  c=8, a=76)
+
+# pp and kp ka combo
+# pehele all pp den all kp
+show(10, 20, d=54, c=43)
+
+#show(a=10, 20, 30, 23)
+# show(10, b=30, a=0, c=30, d=40)
